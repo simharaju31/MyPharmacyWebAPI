e9d8f11ce0da22aadf47c25fb12141a265b6edf1
1000075370 <1000075370@MaverickVM-7>
2023-01-15 13:44:37 +0530

    Added ISavePharmacyService

---
+++ IServices/ISavePharmacyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPharmacyWebAPI.IServices
+{
+    public interface ISavePharmacyService
+    {
+        void SavePharmacy(int memberId, string pharmacyId);
+
+    }
+}
